72c38f4ebf427dc2453dad0eab2105d3dd55e031
sasha wangui <sasha.iluku@gmail.com>
2023-07-03 10:44:54 +0300

    Completed challenge

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # codechallenge-3
+
+# -Usage
+
+~ Click on the "Explore" button to display the list of available movies.
+~ Each movie is represented by a card displaying its poster, title, runtime, showtime, theater capacity, and the number of tickets sold.
+~ Click on the "Buy Ticket" button to purchase a ticket for a specific movie. The number of tickets sold will be updated accordingly.
+~ If the number of tickets sold reaches the theater capacity, the "Tickets sold" message will be updated to "No more Tickets".
+
+# -Technologies Used
+
+HTML
+CSS
+JavaScript
+Fetch API
+
+# -License
+
+This project is licensed under the MIT License.
--- index.js
+++ index.js
@@ -12,9 +12,31 @@ function displayData(movie){
     <h2>${movie.title}</h2>
     <p>${movie.runtime} minutes</p>
     <p> Showtime: ${movie.showtime}</p>
-    <button class="buy-btn" data-id="${movie.id}"> Buy ticket. </button></div>`
+    <p>Theatre capacity ${movie.capacity}</p>
+    <p id="ticketsSold-${movie.id}">Tickets sold : ${movie.tickets_sold}</p>
+    <button id="buy-btn-${movie.id}"> Buy ticket.</button></div>`
     document.querySelector(".container").appendChild(show)
-    console.log(show)
+   
+    // Updating tickets and patching
+    let ticketBtn = document.querySelector(`#buy-btn-${movie.id}`)
+    ticketBtn.addEventListener('click', ()=>{
+        movie.tickets_sold +=1
+       let ticket= document.getElementById(`ticketsSold-${movie.id}`)
+       ticket.innerText= `Tickets sold : ${movie.tickets_sold}`
+      if( movie.tickets_sold >= movie.capacity){
+        ticket.innerText =`Tickets sold : No more Tickets`
+        movie.tickets_sold = movie.capacity
+    
+      }
+         fetch(`http://localhost:3000/films/${movie.id}`,{
+                 method:"PATCH",
+                 headers:{
+                     "Content-Type":"application/json"
+                 },
+                 body:JSON.stringify({tickets_sold: movie.tickets_sold})
+             })
+
+     })
 }
 
 function getData(){
@@ -25,3 +47,5 @@ function getData(){
     })
 }
 getData()
+
+
